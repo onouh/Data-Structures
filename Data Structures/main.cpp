d883a6b3eb189f42ef42ce358391ed5c890b57cf
Omar Nouh <onouh7@gmail.com>
2025-09-27 07:57:32 +0300

    Initial Commit

---
+++ Data Structures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data Structures
+//
+//  Created by Omar Nouh on 9/27/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
